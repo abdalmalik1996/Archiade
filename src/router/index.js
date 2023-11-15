d58ef06b697b43dbf36b398005640821b24445d9
abdalmalik1996 <abd.almalik96@gmail.com>
2023-11-16 01:49:54 +0300

    comp

--- src/router/index.js
+++ src/router/index.js
@@ -9,37 +9,27 @@ const routes = [
       {
         path: "/",
         name: "Home",
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () =>
-          import(/* webpackChunkName: "home" */ "@/views/Home.vue"),
+        component: () => import("@/views/Home.vue"),
       },
       {
         path: "/interior-design",
         name: "interior-design",
-        component: () =>
-          import(
-            /* webpackChunkName: "home" */ "@/views/InteriorDesignViews.vue"
-          ),
+        component: () => import("@/views/InteriorDesignViews.vue"),
       },
       {
         path: "/Showroom",
         name: "Showroom",
-        component: () =>
-          import(/* webpackChunkName: "home" */ "@/views/Showroomviews.vue"),
+        component: () => import("@/views/Showroomviews.vue"),
       },
       {
         path: "/our-brands",
         name: "ourBrands",
-        component: () =>
-          import(/* webpackChunkName: "home" */ "@/views/ourBrandsViews.vue"),
+        component: () => import("@/views/ourBrandsViews.vue"),
       },
       {
         path: "/about-us",
         name: "aboutUs",
-        component: () =>
-          import(/* webpackChunkName: "home" */ "@/views/aboutUsViews.vue"),
+        component: () => import("@/views/aboutUsViews.vue"),
       },
       {
         path: "/Send-Request",
